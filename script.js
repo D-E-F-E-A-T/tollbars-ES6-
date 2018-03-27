2a56824080ac017762c441857297021a7e1645b0
Tobiasz13 <tobiasz-los@wp.pl>
2018-03-27 13:35:39 +0200

    position setting

--- script.js
+++ script.js
@@ -6,20 +6,38 @@ class MWPjs {
         this.position = date.position;
     }
 
+    checkPosition() {
+        if(this.position == 'top') {
+            document.getElementById('bars').style.top = '0';
+        } else if (this.position == 'bottom') {
+            document.getElementById('bars').style.bottom = '0';
+        }
+        console.log('test ?')
+    }
+
     messages() {
         console.log(this.message)
     }
 
-    test() {
-        this.messages();
+    bar() {
         const container = document.createElement('div');
         container.setAttribute('class', 'container');
+        container.setAttribute('id', 'bars');
         document.body.appendChild(container);
+        this.checkPosition()
     }
+
+    topPosition() {
+
+    }
+
+    bottomPosition() {
+
+    }
+
 }
 
 const btn1 = document.getElementById('btn1');
-
     const myBar = new MWPjs({
         message: 'msg here - Beautiful and simple website widgets',
         close: function(){ alert('closed') },
@@ -29,4 +47,4 @@ const btn1 = document.getElementById('btn1');
 
 
 
-btn1.addEventListener("click", () => myBar.test());
+btn1.addEventListener("click", () => myBar.bar());
